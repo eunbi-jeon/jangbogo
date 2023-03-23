7d627a380566f5a89962f8a9741311805d3ed77f
2gyeong <knk222@naver.com>
2023-03-23 11:48:36 +0900

    test 커밋

---
+++ src/main/java/com/jangbogo/controller/ApiController.java
@@ -0,0 +1,21 @@
+package com.jangbogo.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+public class ApiController {
+
+	public static void main(String[] args) {
+		// 테스트
+    }
+}
