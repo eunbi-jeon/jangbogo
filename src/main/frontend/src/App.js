cba50ce45e77fb0492c8da2723061295f4d9e175
2GYEONG\이경 <knk222@naver.com>
2023-03-23 23:30:21 +0900

    api 진행중 - 3

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import PriceInfo from './components/PriceInfo';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
-const App = () => {
-  return (
-    <div>
-      <PriceInfo />
-    </div>
-  );
-};
+function App() {
+   const [hello, setHello] = useState('')
+
+    useEffect(() => {
+        axios.get('/api/hello')
+        .then(response => setHello(response.data))
+        .catch(error => console.log(error))
+    }, []);
+
+    return (
+        <div>
+            백엔드에서 가져온 데이터입니다 : {hello}
+        </div>
+    );
+}
 
 export default App;
