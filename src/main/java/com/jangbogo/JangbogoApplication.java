4e35ca7699e391d9a4852330396d154526207597
eunbi jeon <22.euneu@gmail.com>
2023-03-23 10:36:46 +0900

    초기 셋팅

---
+++ src/main/java/com/jangbogo/JangbogoApplication.java
@@ -0,0 +1,13 @@
+package com.jangbogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JangbogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JangbogoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jangbogo/JangbogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jangbogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JangbogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
