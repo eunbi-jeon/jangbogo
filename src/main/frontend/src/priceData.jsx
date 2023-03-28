20925d676a55def8e06902597f19be1d7ad9fd29
2gyeong <knk222@naver.com>
2023-03-29 00:09:38 +0900

    json data parsing

--- src/main/frontend/src/priceData.jsx
+++ src/main/frontend/src/priceData.jsx
@@ -1,68 +1,28 @@
-import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function PriceData() {
-  const [priceData, setPriceData] = useState([]);
+function Price() {
+  const [data, setData] = useState(null);
 
   useEffect(() => {
     axios.get('/price')
-      .then(response => {
-        const { data } = response;
-        const filteredData = data.data.item.map((item) => {
-          return {
-            itemcode: item.item_code,
-            item_name: item.item_name,
-            unit: item.unit,
-            day1: item.day1,
-            dpr1: item.dpr1,
-            day2: item.day2,
-            dpr2: item.dpr2,
-            day3: item.day3,
-            dpr3: item.dpr3
-          };
-        });
-        setPriceData(filteredData);
-      })
-      .catch(error => {
-        console.log(error);
-      })
+      .then(item => setData(item.data))
+      .catch(error => console.error(error));
   }, []);
+
+  if (!data) return <div>Loading...</div>;
 
   return (
     <div>
-      <h1>Price Data</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>품목명</th>
-            <th>품목코드</th>
-            <th>단위</th>
-            <th>조회일자</th>
-            <th>조회일자 가격</th>
-            <th>1일전 일자</th>
-            <th>1일전 가격</th>
-            <th>1주일전 일자</th>
-            <th>1주일전 가격</th>
-          </tr>
-        </thead>
-        <tbody>
-          {priceData.map((data, index) => (
-            <tr key={index}>
-              <td>{data.item_name}</td>
-              <td>{data.itemcode}</td>
-              <td>{data.unit}</td>
-              <td>{data.day1}</td>
-              <td>{data.dpr1}</td>
-              <td>{data.day2}</td>
-              <td>{data.dpr2}</td>
-              <td>{data.day3}</td>
-              <td>{data.dpr3}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      {data.item.map((item, index) => (
+        <div key={index}>
+          <h3>{item.item_name}</h3>
+          <p>품목코드: {item.item_code}</p>
+          <p>단위: {item.unit}</p>
+          <p>조회일자: {item.day1}, 가격: {item.dpr1}</p>
+          <p>1일전 일자: {item.day2}, 가격: {item.dpr2}</p>
+          <p>1주일전 일자: {item.day3}, 가격: {item.dpr3}</p>
+        </div>
+      ))}
     </div>
   );
 }
-
-export default PriceData;
--- src/main/java/com/jangbogo/controller/PriceController.java
+++ src/main/java/com/jangbogo/controller/PriceController.java
@@ -12,11 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+
+
 @RestController
 public class PriceController {
 
     @GetMapping("/api/price")
-    public ResponseEntity<String> getPrice() {
+    public ResponseEntity<JsonNode> getPrice() throws JsonMappingException, JsonProcessingException  {
 
         // 실행 시 시간으로 데이터 불러옴 (주말인 경우 그 주의 금요일 정보 출력)
     	 LocalDate date = LocalDate.now();
@@ -37,6 +45,7 @@ public class PriceController {
         String countryCode = "1101";
         String regday = startDate ;
         String convertKgYn = "Y";
+        
 
         RestTemplate restTemplate = new RestTemplateBuilder()
                 .setConnectTimeout(Duration.ofSeconds(10))
@@ -55,16 +64,58 @@ public class PriceController {
                 .queryParam("p_convert_kg_yn", convertKgYn);
 
         ResponseEntity<String> response = restTemplate.getForEntity(builder.toUriString(), String.class);
+        
 
+        // 연결 성공 
         if (response.getStatusCode().is2xxSuccessful()) {
         	
         	
+        	// 데이터 파싱 
+        	String jsondata = response.getBody();
+        	ObjectMapper objectMapper = new ObjectMapper();
+        	JsonNode rootNode = objectMapper.readTree(jsondata);
+        	JsonNode itemNode = rootNode.path("data").path("item");
         	
-            return response;
+
+        	System.out.println(itemNode);
+        	
+        	/*
+        	for (JsonNode item : itemNode) {
+        	    System.out.println("item_name: " + item.get("item_name").asText());
+        	    System.out.println("item_code: " + item.get("item_code").asText());
+        	    System.out.println("kind_name: " + item.get("kind_name").asText());
+        	    System.out.println("kind_code: " + item.get("kind_code").asText());
+        	    System.out.println("rank: " + item.get("rank").asText());
+        	    System.out.println("rank_code: " + item.get("rank_code").asText());
+        	    System.out.println("unit: " + item.get("unit").asText());
+        	    System.out.println("day1: " + item.get("day1").asText());
+        	    System.out.println("dpr1: " + item.get("dpr1").asText());
+        	    System.out.println("day2: " + item.get("day2").asText());
+        	    System.out.println("dpr2: " + item.get("dpr2").asText());
+        	    System.out.println("day3: " + item.get("day3").asText());
+        	    System.out.println("dpr3: " + item.get("dpr3").asText());
+        	    System.out.println("day4: " + item.get("day4").asText());
+        	    System.out.println("dpr4: " + item.get("dpr4").asText());
+        	    System.out.println("day5: " + item.get("day5").asText());
+        	    System.out.println("dpr5: " + item.get("dpr5").asText());
+        	    System.out.println("day6: " + item.get("day6").asText());
+        	    System.out.println("dpr6: " + item.get("dpr6").asText());
+        	    System.out.println("day7: " + item.get("day7").asText());
+        	    System.out.println("dpr7: " + item.get("dpr7").asText());
+        	}
+        	*/
+
+
             
+        	return ResponseEntity.ok(itemNode);
+
+            
+        // 연결 실패	 
         } else {
-            return ResponseEntity.status(HttpStatus.SC_INTERNAL_SERVER_ERROR).body("Request failed");
+        	return ResponseEntity.status(HttpStatus.SC_INTERNAL_SERVER_ERROR).body(null);
         }
+        
+        
     }
     
     
