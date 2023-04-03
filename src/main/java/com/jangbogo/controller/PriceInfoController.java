1b9c538de617811eee57fa1d10fd9d17025cbec8
2gyeong <knk222@naver.com>
2023-04-03 10:26:08 +0900

    main view 수정

--- src/main/java/com/jangbogo/controller/PriceInfoController.java
+++ src/main/java/com/jangbogo/controller/PriceInfoController.java
@@ -3,12 +3,15 @@ package com.jangbogo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jangbogo.dto.PriceInfoDTO;
 import com.jangbogo.repository.PriceInfoRepository;
 import com.jangbogo.service.PriceInfoService;
@@ -41,9 +44,7 @@ public class PriceInfoController {
     	  priceInfoRepository.saveAll(priceList);
         
     	// 저장된 DB 반환 
-        List<PriceInfoDTO> priceInfo = priceInfoService.getPriceInfoList();
-        
-        return priceInfo;  
+		return priceInfoService.getPriceInfoList();
     }
     
 }
--- src/main/java/com/jangbogo/dto/PriceInfoDTO.java
+++ src/main/java/com/jangbogo/dto/PriceInfoDTO.java
@@ -69,6 +69,8 @@ public class PriceInfoDTO {
 	    this.regDay = startDate; 
 		
 	}
+	
+	public PriceInfoDTO() {}
 
 
 
