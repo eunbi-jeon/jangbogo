2f19a9f8cc3aebfca2c9932864f091afb68e9673
2gyeong <knk222@naver.com>
2023-04-06 01:30:58 +0900

    db 중복값 체크 후 저장

--- src/main/java/com/jangbogo/controller/PriceInfoController.java
+++ src/main/java/com/jangbogo/controller/PriceInfoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,22 +37,35 @@ public class PriceInfoController {
 	@Autowired
 	private PriceInfoService priceInfoService;
 
-   
-    
+
+    @Scheduled(cron = "0 0 17 * * ?")
     @PostMapping("/")
-    public void getPrice() throws JsonMappingException, JsonProcessingException{ 
-    	
-    	// Open Api data DB에 저장   
-    	List<PriceInfo> priceList = priceInfoJsonParser.parsePriceInfo();
-    	  priceInfoRepository.saveAll(priceList);
-        
-    	// 저장된 DB 반환 
-	//	return priceInfoService.getPriceInfoList();
+    public void getPrice() throws JsonMappingException, JsonProcessingException{
+
+        List<PriceInfo> priceList = priceInfoJsonParser.parsePriceInfo();
+
+        if (priceInfoRepository.count() == 0) {
+            // DB에 저장된 데이터가 없는 경우
+            priceInfoRepository.saveAll(priceList);
+        } else {
+            // DB에 저장된 데이터가 있는 경우 중복 체크 후 저장
+            for (PriceInfo priceInfo : priceList) {
+                String regDay = priceInfoJsonParser.getStartDate();
+                List<PriceInfo> existingPriceInfos = priceInfoRepository.findByRegDay(regDay);
+
+                if (existingPriceInfos.isEmpty()) {
+                    // 새로운 데이터 저장
+                    priceInfoRepository.save(priceInfo);
+                }
+            }
+        }
+
     }
 
-  
+
+
     @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody String savePriceInfo() throws JsonProcessingException {
+    public @ResponseBody String jsonPriceInfo() throws JsonProcessingException {
         Gson gson = new Gson();
         String jsonString = gson.toJson(priceInfoService.getPriceInfoList());
         System.err.println(jsonString);
