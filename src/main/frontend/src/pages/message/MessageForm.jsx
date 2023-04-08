6a9ea2ffcd90d19407b116960dbcfc8185b07ef1
jisu kim <yyjisu@gmail.com>
2023-04-08 16:23:39 +0900

    쪽지 폼 모달 구현

--- src/main/frontend/src/pages/message/MessageForm.jsx
+++ src/main/frontend/src/pages/message/MessageForm.jsx
@@ -6,6 +6,7 @@ function MessageForm() {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [receiverNickname, setReceiver] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const searchName = (e) => {
     console.log(e.target.value);
@@ -19,7 +20,7 @@ function MessageForm() {
     alert('빈칸을 모두 채워주세요');
     return;
     }
-
+    
     axios
       .post(
         '/api/messages',
@@ -35,7 +36,6 @@ function MessageForm() {
         setContent('');
         setReceiver('');
         console.log('Message saved successfully');
-        window.location.href = '/messages/postbox/receiver';
       })
       .catch((error) => {
         console.error(error);
@@ -44,46 +44,49 @@ function MessageForm() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className='wrap'>
-        <div>
-          <h1>새 쪽지</h1>
-          <hr />
-        </div>
-        <div id='message_form'>
-          <div>
-            <label htmlFor='receiverNickname'></label>
-            <input
+      <div className="modal" style={{ display: isModalOpen ? 'block' : 'none' }}>
+        <h2>새 쪽지</h2>
+        <hr/>
+        <div id='wrap'>
+        <label htmlFor='receiverNickname'></label>
+        <input
               type='text'
               placeholder='받는 사람의 닉네임을 입력해주세요'
               id='receiverNickname'
               value={receiverNickname}
-              onChange={searchName}
-            />
-          </div>
-          <hr />
-          <div>
-            <label htmlFor='title'></label>
-            <input
+              onChange={searchName}/>
+        <label htmlFor='title'></label>
+        <input
               placeholder='쪽지 제목을 입력하세요'
               type='text'
               id='title'
               value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor='content'></label>
-            <textarea
+              onChange={(e) => setTitle(e.target.value)} />
+        <label htmlFor='content'></label>
+        <textarea
               placeholder='쪽지 내용을 입력하세요'
               id='content'
               value={content}
-              onChange={(e) => setContent(e.target.value)}
-            />
+              onChange={(e) => setContent(e.target.value)}/>
+          <button type="submit">보내기</button>
+        </div>
+        <button className="close-btn" onClick={() => setIsModalOpen(false)}>닫기</button>
+      </div>
+      <div className="wrap">
+        <div>
+          <h1>쪽지 기능</h1>
+          <hr />
+        </div>
+        <div id="message_form">
+          <div>
+            <p className="open-btn" onClick={() => setIsModalOpen(true)}>쪽지 보내기</p>
+          </div>
+          <div>
+            <form onSubmit={handleSubmit}>
+              {/* Form input elements */}
+            </form>
           </div>
         </div>
-        <button type='submit'>Send Message</button>
-        <button type="button" onClick={() => { window.location.href = '/messages/postbox/receiver'; }}>받은 쪽지 목록</button>
-        <button type="button" onClick={() => { window.location.href = '/messages/postbox/sender'; }}>보낸 쪽지 목록</button>
       </div>
     </form>
   );
