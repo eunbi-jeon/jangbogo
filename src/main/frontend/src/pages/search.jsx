21182d6883c3b92d09894508eb02a9f7256e9572
HWANG INJAE <sub.ij002@gmail.com>
2023-04-08 12:51:46 +0900

    ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ

--- src/main/frontend/src/pages/search.jsx
+++ src/main/frontend/src/pages/search.jsx
@@ -1,4 +1,3 @@
-
 import React, {useState, useEffect} from 'react';
 import '../css/search.css'
 import axios from 'axios';
@@ -6,105 +5,99 @@ import Save from '../components/Save';
 
 
 function numberWithCommas(pNumber) {
-    return pNumber.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return pNumber.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 function Search(props) {
   const query = props.query;
+  const authenticated = props.authenticated;
   const [items, setItems] = useState([]);
-  const [sortBy, setSortBy] = useState('관련도순');
+  const [sortBy, setSortBy] = useState('sim');
+  const [start, setStart] = useState(1);
+  const [display, setDisplay] = useState(99);
+
+
   const boldText = (text) => {
     return text.replaceAll(query, `<b>${query}</b>`);
   };
 
-  useEffect(() => {
-    async function fetchData(){
-      if (query) {
-		 
-          try {
-            const encodedQuery = encodeURIComponent(query);
-       
-            let url = `/api/search?query=${encodedQuery}`;
-            
-            if (query) {
-              url += '&category1=식품'; // query 값이 있을 때만 category1 파라미터를 추가
-            }
-            
-            
-            if (sortBy === '낮은 가격순') {
-              url += '&sort=asc';
-            } else if (sortBy === '관련도순') {
-                url += '&sort=sim';
-            } else if (sortBy === '최신순'){
-              url += '&sort=date';
-            }
-            const response = await axios.get(url);
-            const data = await response.data;
-            const filteredData = data.filter(item => item.category1 === '식품');
-            const sortedData = sortBy === 'asc' ? filteredData.sort((a, b) => a.lprice - b.lprice) : sortBy === 'sim' ? filteredData.sort((a, b) => b.mallScore - a.mallScore) : filteredData.sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate));
-            setItems(sortedData);
-            console.log(sortedData);
-            
-            if (sortedData.length === 0) {
-             	alert("검색 결과가 없습니다.");
-         	}
-  
-          } catch (error) {
-            console.error(error);
-            setItems([]);
-            alert("검색 결과가 없습니다.");
-          }
-        } else {
-          setItems([]);
-         
+  const fetchData = async () => {
+    if (query) {
+      try {
+        const encodedQuery = encodeURIComponent(query);
+        let url = `/api/search?query=${encodedQuery}&start=${start}&display=${display}`;
+        if (query) {
+          url += '&category1=식품';
         }
+        if (sortBy === 'asc') {
+          url += '&sort=asc';
+        } else if (sortBy === 'sim') {
+          url += '&sort=sim';
+        } else if (sortBy === 'date') {
+          url += '&sort=date';
+        }
+        const response = await axios.get(url);
+        const data = await response.data;
+        const filteredData = data.filter(item => item.category1 === '식품');
+        const sortedData = sortBy === 'asc' ? 
+        filteredData.sort((a, b) => a.lprice - b.lprice) 
+        : sortBy === 'sim' ? filteredData.sort((a, b) => b.mallScore - a.mallScore) 
+        : filteredData.sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate));
+        
+        setItems(prevItems=>[...prevItems, ...sortedData]);
+        
+        if (sortedData.length === 0) {
+          alert("검색 결과가 없습니다.");
+        }
+      } catch (error) {
+        console.error(error);
+        setItems([]);
+        alert("검색 중 오류가 발생했습니다. 다시 시도해주세요.");
       }
-      fetchData()
-  }, [query, sortBy]);
-  
-  
-  const onClickItem=(item, e)=>{
+    } else {
+      setItems([]);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [query, sortBy, start, display]);
+
+  const loadMore=()=>{
+    setStart(start => start + display);
+  }
+
+  const onClickItem = (item, e) => {
     if (!e || !e.target || e.target.nodeName !== "BUTTON") {
       window.open(item.link, "_blank");
-    }else{
-		e.preventDefault();
-	}
-  } 
-  
+    } else {
+      e.preventDefault();
+    }
+  };
 
   const handleSortByChange = (e) => {
     const selectedValue = e.target.value;
-    switch (selectedValue) {
-      case "관련도순":
-        setSortBy("sim");
-        break;
-      case "최신순":
-        setSortBy("date");
-        break;
-      case "낮은가격순":
-        setSortBy("asc");
-        break;
-      default: break;
+   
+      setSortBy(selectedValue);
     }
-    setSortBy(selectedValue);
-};
-  
-
 
     return (
       <>
       <div className="search-container">
+      <div className="searchlist-top">
+        <div className="countItem"> 검색 결과 ({items.length >= 99 ? '99+' : items.length})</div>
         <div className='sort-box'>
-
           <label htmlFor="sortOrder"></label>
           <select id="sortBy" name="sortBy" value={sortBy} onChange={handleSortByChange}>
             <option value="sim">관련도순</option>
-            <option value="asc">낮은 가격순</option>
+            <option value="asc">낮은가격순</option>
             <option value="date">최신순</option>
           </select>
         </div>
+      </div>
+      <div className="listline"></div>
         <ul>    
-          {items.slice(0, 30).map(item => (
+          {items.map(item => (
             <li key={item.link}>
               <div className='search-item' onClick={(e)=>onClickItem(item, e)} >
                 <div className='search-item-left'>
@@ -115,17 +108,19 @@ function Search(props) {
                   <div className='price'>
                     {numberWithCommas(item.lprice)}
                     <span className='unit'>원</span>
-                 
+                    <Save authenticated={authenticated} onClick={(e) => {e.stopPropagation()}} item={item} />
                 </div>
               </div>
-              <div className='search-itemDto-right'>
-    <Save onClick={(e) => {e.stopPropagation(); props.handleSave(item)}}
-                    item={item} />
-              </div>
+
             </div>
           </li>
         ))}
+        {items.length > 0  && (
+        <button onClick={loadMore} className='findMorebtn'>더 보기</button>
+        )}
       </ul>
+    
+          
     </div>
     </>
   );
